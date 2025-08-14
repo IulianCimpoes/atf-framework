c93276a1a1193060b8c2d9655d2acf0a7aff7861
iucimpoes <iulikyo@gmail.com>
2025-08-14 18:21:49 +0300

    add prop and ScenarioScope

--- ui/src/main/java/com/atf/ui/config/WebDriverConfig.java
+++ ui/src/main/java/com/atf/ui/config/WebDriverConfig.java
@@ -1,18 +1,30 @@
 package com.atf.ui.config;
 
+import io.cucumber.spring.ScenarioScope;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class WebDriverConfig {
 
+    @Value("${ui.headless:true}")
+    private boolean headless;
+
     @Bean(destroyMethod = "quit")
+    @ScenarioScope
     public WebDriver webDriver() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
+        if (headless) {
+            options.addArguments("--headless=new");
+            options.addArguments("--window-size=1280,900");
+        } else {
+            options.addArguments("--start-maximized");
+        }
         return new ChromeDriver(options);
     }
 }
