bb7170303ba48164ce8ee203fb165417bd2be434
iucimpoes <iulikyo@gmail.com>
2025-08-15 15:26:29 +0300

    warnings fixed

--- test/src/test/java/com/atf/test/runner/TestRunner.java
+++ test/src/test/java/com/atf/test/runner/TestRunner.java
@@ -1,8 +1,16 @@
 package com.atf.test.runner;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.*;
 
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 
-@Cucumber
+@Suite
+@IncludeEngines("cucumber")
+@SelectPackages("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.atf.test")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:target/cucumber-report.html, json:target/cucumber.json")
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@smoke")
 public class TestRunner {
 }
