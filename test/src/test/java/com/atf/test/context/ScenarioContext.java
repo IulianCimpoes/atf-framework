f3c8d585b998ec56d81189ead1c8a6c9b1d16a90
iucimpoes <iulikyo@gmail.com>
2025-08-18 12:40:19 +0300

    add debug logs to ScenarioContext

--- test/src/test/java/com/atf/test/context/ScenarioContext.java
+++ test/src/test/java/com/atf/test/context/ScenarioContext.java
@@ -1,6 +1,7 @@
 package com.atf.test.context;
 
 import io.cucumber.spring.ScenarioScope;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -8,24 +9,31 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 @ScenarioScope
+@Slf4j
 public class ScenarioContext {
 
     private final Map<String, Object> store = new ConcurrentHashMap<>();
 
     public <T> void put(String key, T value) {
         store.put(key, value);
+        log.debug("[ScenarioContext] put key: {} value: {}", key, value);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T get(String key) {
-        return (T) store.get(key);
+        T value = (T) store.get(key);
+        log.debug("[ScenarioContext] get key: {} value: {}", key, value);
+        return value;
     }
 
     public boolean has(String key) {
-        return store.containsKey(key);
+        boolean present = store.containsKey(key);
+        log.debug("[ScenarioContext] has key: {} present: {}", key, present);
+        return present;
     }
 
     public void clear() {
         store.clear();
+        log.debug("[ScenarioContext] cleared all entries");
     }
 }
